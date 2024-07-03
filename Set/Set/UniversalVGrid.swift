2eaa03565e81e9d03fe6222653ff5150e72c25a4
Gl0ukhov <gmv96@mail.ru>
2024-07-03 14:30:13 +0300

    Creating a deck of cards and embedding them in the view

---
+++ Set/Set/UniversalVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  UniversalVGrid.swift
+//  Set
+//
+//  Created by Матвей Глухов on 03.07.2024.
+//
+
+import SwiftUI
+
+struct UniversalVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UniversalVGrid()
+}
